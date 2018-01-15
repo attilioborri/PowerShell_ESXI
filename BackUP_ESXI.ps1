af395dbee3423f509a77f3938bb157418b9c4cdb
Attilio Borri <attilioborri@gmail.com>
2018-01-15 18:25:37 +0100

    - Added Copy to network disk

--- BackUP_ESXI.ps1
+++ BackUP_ESXI.ps1
@@ -9,6 +9,9 @@
 # 
 # Install-Module -Name VMware.PowerCLI
 
+$BKPESXI_EnableDateRename = 1;
+#$BKPESXI_MAXBKP = 5; 
+
 $s=Get-Date;
 $BackUP_ESXI_Temp_File = New-TemporaryFile;
 $BackUP_ESXI_Host = $args[0];
@@ -20,28 +23,74 @@ if($BackUP_ESXI_CountParameters.Equals(2)){
     $BackUP_ESXI_Server = Connect-VIServer -Server $BackUP_ESXI_Host;
     $BackUP_ESXI_executebackup = 1;
 }elseif($BackUP_ESXI_CountParameters.Equals(4)){
-    #echo 'AutomaticLogin'
+    #echo 'AutomaticLogin-LocalFolder'
     $BackUP_ESXI_Username = $args[1];
     $BackUP_ESXI_Password = $args[2];
     $BackUP_ESXI_DefaultPath = $args[3];
     $BackUP_ESXI_Server = Connect-VIServer -Server $BackUP_ESXI_Host -User $BackUP_ESXI_Username -Password $BackUP_ESXI_Password;
     $BackUP_ESXI_executebackup = 1;
+}elseif($BackUP_ESXI_CountParameters.Equals(6)){
+    #echo 'AutomaticLogin-NetWorkDrive'
+    $BackUP_ESXI_Username = $args[1];
+    $BackUP_ESXI_Password = $args[2];
+    $BackUP_ESXI_DefaultPath = $args[3];
+    $BackUP_ESXI_Server = Connect-VIServer -Server $BackUP_ESXI_Host -User $BackUP_ESXI_Username -Password $BackUP_ESXI_Password;
+    $BackUP_ESXI_executebackup = 1;
+    $BackUP_ESXI_NetUsername = $args[4];
+    $BackUP_EDCI_NetPassword = $args[5];
 }else{
     $BackUP_ESXI_executebackup = 0;
 }
 if($BackUP_ESXI_executebackup.Equals(1)){
-    Get-VMHostFirmware -VMHost $BackUP_ESXI_Host -BackupConfiguration -DestinationPath $BackUP_ESXI_DefaultPath;
-    Disconnect-VIServer -Server $BackUP_ESXI_Server -confirm:$false;
+   if($BackUP_ESXI_DefaultPath.ToString().Contains('\\') -and $BackUP_ESXI_CountParameters.Equals(6)){
+        #network drive
+        $LocalTempFolder = "C:\ESXiBackUP"
+        #New-Item -ItemType directory -Path $LocalTempFolder
+        
+        Get-VMHostFirmware -VMHost $BackUP_ESXI_Host -BackupConfiguration -DestinationPath $LocalTempFolder;
+        Disconnect-VIServer -Server $BackUP_ESXI_Server -confirm:$false;
+        net use $BackUP_ESXI_DefaultPath /user:$BackUP_ESXI_NetUsername $BackUP_EDCI_NetPassword;
+        if($BKPESXI_EnableDateRename.Equals(1)){
+            $files = Get-ChildItem $LocalTempFolder;
+            for($i=0;$i -lt $files.Count; $i++){
+                if(Test-Path $files[$i].FullName){
+                    $GetDate = Get-Date -format "yyyy/MM/dd_HHmm";
+                    $newfile = "$($LocalTempFolder)\$($files[$i].BaseName) $($GetDate)$($files[$i].Extension)".Replace(" ","_").Replace("/","-");
+
+                    #echo $newfile;
+                    #echo $($files[$i].FullName);
+
+                    echo "Move-Item -Path $($files[$i].FullName) -Destination $newfile";
+
+                    Move-Item -Path "$($files[$i].FullName)" -Destination "$newfile";
+                    Move-Item $newfile $BackUP_ESXI_DefaultPath;
+                }
+            }
+        }
+    }else{
+        #local folder
+        Get-VMHostFirmware -VMHost $BackUP_ESXI_Host -BackupConfiguration -DestinationPath $BackUP_ESXI_DefaultPath;
+        Disconnect-VIServer -Server $BackUP_ESXI_Server -confirm:$false;
+    }
+
+    
+
+
 }else{
     echo '';
     echo 'Wrong number of parameters. Following modes are allowed:';
     echo '';
-    echo 'Automatic Login:';
+    echo 'Automatic Login Local Folder:';
     echo '      BackUP_ESXI.ps1 <ip_address> <username> <password> <default_path>';
+    echo '';
+    echo 'Automatic Login Network Drive:';
+    echo '      BackUP_ESXI.ps1 <ip_address> <username> <password> <network path> <net_username> <net_password>';
     echo '';
     echo 'Custom Login';
     echo '      BackUP_ESXI.ps1 <ip_address> <default_path>';
 }
+
+
 
 
 #Delete Temp File before end of the script
